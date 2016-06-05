0ebb79c4d2d3fc9c9bc67d8a510830876367ad04
Redid <dwdsuder@gmail.com>
2016-06-05 17:27:21 +0200

    Basic viewer. Points from file

---
+++ project/viewer/Viewer/Point.cs
@@ -0,0 +1,53 @@
+﻿using SharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Viewer
+{
+    public class Point
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public float r = 1;
+        public float g = 1;
+        public float b = 1;
+
+        public Point(String stringformatPoint)
+        {
+            string[] xyz = stringformatPoint.Split(',');
+            try
+            {
+                this.x = float.Parse(xyz[0]);
+                this.y = float.Parse(xyz[1]);
+                this.z = float.Parse(xyz[2]);
+            }
+            catch
+            {
+                throw new FormatException();
+            }
+        }
+
+        public Point(float x, float y, float z)
+        {
+            this.x = x; this.y = y; this.z = z;
+        }
+
+        public void draw(OpenGL gl)
+        {
+            gl.PointSize(2);
+            gl.Begin(OpenGL.GL_POINTS);
+            gl.Color(r, g, b);
+            gl.Vertex(x, y, z);
+            gl.End();
+        }
+
+        public void setColor(float r, float g, float b)
+        {
+            this.r = r; this.g = g; this.b = b;
+        }
+    }
+}
---
+++ project/viewer/Viewer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Viewer
+{
+    public static class Program
+    {
+        private static string filepath;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: [filepath]");
+                Environment.Exit(-1);
+            }
+            filepath = args[0];
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            Application.Run(new SharpGLForm(readFileForPoints()));
+        }
+
+        private static Point[] readFileForPoints()
+        {
+            Point[] points = null;
+            try
+            {
+                string[] pointsString = System.IO.File.ReadAllLines(@filepath);
+                points = new Point[pointsString.Length];
+                for (int i = 0; i < pointsString.Length; i++)
+                {
+                    points[i] = new Point(pointsString[i]);
+                }
+            }
+            catch
+            {
+                Environment.Exit(-1);
+            }
+            return points;
+        }
+    }
+}
